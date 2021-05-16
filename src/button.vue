df263932919242ee028dbaf0c285ec4c87a40dc3
botshen <363088847@qq.com>
2021-05-16 09:12:09 +0800

    parcel 自闭合引起的 bug 解决

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,6 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-    <g-icon class="icon" v-if="icon" :name="icon"/>
+    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
     <g-icon class="loading" name="loading"></g-icon>
     <div class="content">
       <slot/>
