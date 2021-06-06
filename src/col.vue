bc3d1de80cdd20cc8e8c144a916d0119e6e28ca5
botshen <363088847@qq.com>
2021-06-06 18:48:55 +0800

    添加 row 的 align=left/right/center 属性

--- src/col.vue
+++ src/col.vue
@@ -5,14 +5,14 @@
 </template>
 <script>
 export default {
-  name: "GuLuCol",
+  name: "GuluCol",
   props: {
     span: {
       type: [Number, String]
     },
     offset: {
       type: [Number, String]
-    }
+    },
   },
   data() {
     return {
@@ -30,30 +30,24 @@ export default {
     colStyle() {
       return {
         paddingLeft: this.gutter / 2 + "px",
-        paddingRight: this.gutter / 2 + "px"
+        paddingRight: this.gutter / 2 + "px",
       }
     }
   }
-
 }
 </script>
 <style scoped lang="scss">
 .col {
-  height: 100px;
-  background: grey;
-  width: 50%;
-  border: 1px solid red;
-  margin: 0 10px;
   $class-prefix: col-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
-      width: ($n/24) * 100%;
+      width: ($n / 24) * 100%;
     }
   }
   $class-prefix: offset-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
-      margin-left: ($n/24) * 100%;
+      margin-left: ($n / 24) * 100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,32 +1,50 @@
 <template>
-  <div class="row" :style="rowStyle">
+  <div class="row" :style="rowStyle" :class="rowClass">
     <slot></slot>
   </div>
 </template>
 <script>
 export default {
-  name: "GuLuRow",
+  name: 'GuluRow',
   props: {
     gutter: {
       type: [Number, String]
+    },
+    align: {
+      type: String,
+      validator (value) {
+        return ['left', 'right', 'center'].includes(value)
+      }
     }
   },
   computed: {
-    rowStyle() {
+    rowStyle () {
       let {gutter} = this
-      return {marginLeft: -gutter / 2 + "px", marginRight: -gutter / 2 + "px"}
+      return {marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}
+    },
+    rowClass () {
+      let {align} = this
+      return [align && `align-${align}`]
     }
   },
-  mounted() {
+  mounted () {
     this.$children.forEach((vm) => {
       vm.gutter = this.gutter
     })
   }
 }
 </script>
-<style lang="scss" scoped>
-.row {
+<style scoped lang="scss">
+.row{
   display: flex;
-  margin: 0 -10px;
+  &.align-left {
+    justify-content: flex-start;
+  }
+  &.align-right {
+    justify-content: flex-end;
+  }
+  &.align-center {
+    justify-content: center;
+  }
 }
 </style>
