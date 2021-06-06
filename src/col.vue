79b5eef9e7792cb8e64efbc6d853140b98bb0875
botshen <363088847@qq.com>
2021-06-06 16:54:04 +0800

    重构 row 和 col

--- src/col.vue
+++ src/col.vue
@@ -1,17 +1,11 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`,offset && `offset-${offset}`]"
-       :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
+  <div class="col" :class="colClass" :style="colStyle">
     <slot></slot>
   </div>
 </template>
 <script>
 export default {
   name: "GuLuCol",
-  data() {
-    return {
-      gutter: 0
-    }
-  },
   props: {
     span: {
       type: [Number, String]
@@ -20,12 +14,27 @@ export default {
       type: [Number, String]
     }
   },
-  created() {
-    console.log("col create")
+  data() {
+    return {
+      gutter: 0,
+    }
   },
-  mounted() {
-    console.log("col mounted")
+  computed: {
+    colClass() {
+      let {span, offset} = this
+      return [
+        span && `col-${span}`,
+        offset && `offset-${offset}`
+      ]
+    },
+    colStyle() {
+      return {
+        paddingLeft: this.gutter / 2 + "px",
+        paddingRight: this.gutter / 2 + "px"
+      }
+    }
   }
+
 }
 </script>
 <style scoped lang="scss">
