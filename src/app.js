2efd7061fe0d65c535ebf3e7d6a56e3716ba17b7
botshen <363088847@qq.com>
2021-06-07 23:25:52 +0800

    响应式基本完成

--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@ import Vue from "vue"
 import Button from "./button"
 import Icon from "./icon"
 import ButtonGroup from "./button-group"
-import input from "./input"
+import Input from "./input"
 import Row from "./row"
 import Col from "./col"
-//全局组件
+
 Vue.component("g-button", Button)
 Vue.component("g-icon", Icon)
 Vue.component("g-button-group", ButtonGroup)
-Vue.component("g-input", input)
-Vue.component("g-col", Col)
+Vue.component("g-input", Input)
 Vue.component("g-row", Row)
+Vue.component("g-col", Col)
 
 new Vue({
   el: "#app",
@@ -22,17 +22,10 @@ new Vue({
     message: "hi"
   },
   created() {
-    setTimeout(() => {
-      let event = new Event("change")
-      let inputElement = this.$el.querySelector("input")
-      inputElement.dispatchEvent(event)
-    }, 3000)
   },
   methods: {
     inputChange(e) {
       console.log(e)
     }
   }
-
 })
-
