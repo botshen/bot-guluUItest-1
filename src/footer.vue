6e6f5281cd610f7c24b931f7d983796741bd334a
botshen <363088847@qq.com>
2021-06-08 22:29:16 +0800

    创建 layout content  footer sider 组件

---
+++ src/footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'footer'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="layout">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'layout'
+};
+</script>
+
+<style lang="scss" scoped>
+.layout {
+
+}
+</style>
