7a8c63611bc666477ea1512fa06aa8f9b2337d3d
botshen <363088847@qq.com>
2021-06-11 22:16:29 +0800

    添加简单动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="sider" v-if="visible">
-    <slot></slot>
-    <button @click="visible=false">close</button>
-  </div>
+  <transition name="slide">
+    <div class="sider" v-if="visible">
+      <slot></slot>
+      <button @click="visible = false">close</button>
+    </div>
+  </transition>
 </template>
 <script>
 export default {
-  name: 'GuluSider',
+  name: "GuluSider",
   data() {
     return {
-      visible: true
-    }
+      visible: true,
+    };
   },
-  methods: {}
-}
+  methods: {},
+};
 </script>
 
 <style lang="scss" scoped>
@@ -24,5 +26,13 @@ export default {
     top: 0;
     right: 0;
   }
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.5s;
+}
+.slide-enter,
+.slide-leave-to {
+  margin-left: -200px;
 }
 </style>
