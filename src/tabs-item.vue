5f96278881ae805f0894c6e891fee16b3d29f086
shenxin <363088847@qq.com>
2021-08-06 23:28:55 +0800

    添加部分测试用例

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item" @click="onClick" :class="classes">
+  <div class="tabs-item" @click="onClick" :class="classes" :data-name="name">
     <slot></slot>
   </div>
 </template>
@@ -31,34 +31,37 @@ export default {
     }
   },
   created() {
-    this.eventBus.$on('update:selected', (name) => {
-      this.active = name === this.name;
-    })
+    if (this.eventBus) {
+      this.eventBus.$on('update:selected', (name) => {
+        this.active = name === this.name;
+      })
+    }
   },
   methods: {
     onClick() {
       if (this.disabled) {
         return
       }
-      this.eventBus.$emit('update:selected', this.name, this)
+      this.eventBus && this.eventBus.$emit('update:selected', this.name, this)
+      this.$emit('click', this)
     }
   }
 }
 </script>
 <style lang="scss" scoped>
-$blue: #1890ff;
+$blue: blue;
 $disabled-text-color: grey;
 .tabs-item {
-  display: flex;
   flex-shrink: 0;
   padding: 0 1em;
   cursor: pointer;
   height: 100%;
+  display: flex;
   align-items: center;
 
   &.active {
-    font-weight: bold;
     color: $blue;
+    font-weight: bold;
   }
 
   &.disabled {
