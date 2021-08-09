86696697a0c8e72e340f78c938f7da05abe83961
Xin shen <363088847@qq.com>
2021-08-09 22:09:43 +0800

    添加 single 选项

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="collapseItem">
-    <div class="title" @click="open=!open">
-      {{title}}
+    <div class="title" @click="toggle">
+      {{ title }}
     </div>
     <div class="content" v-if="open">
       <slot></slot>
@@ -18,9 +18,30 @@ export default {
       required: true
     }
   },
-  data () {
+  data() {
     return {
       open: false
+    }
+  },
+  inject: ['eventBus'],
+  mounted() {
+    this.eventBus && this.eventBus.$on('update:selected', (vm) => {
+      if (vm !== this) {
+        this.close()
+      }
+    })
+  },
+  methods: {
+    toggle() {
+      if (this.open) {
+        this.open = false
+      } else {
+        this.open = true
+        this.eventBus && this.eventBus.$emit('update:selected', this)
+      }
+    },
+    close() {
+      this.open = false
     }
   }
 }
@@ -40,18 +61,21 @@ $border-radius: 4px;
     align-items: center;
     padding: 0 8px;
   }
+
   &:first-child {
     > .title {
       border-top-left-radius: $border-radius;
       border-top-right-radius: $border-radius;
     }
   }
+
   &:last-child {
     > .title:last-child {
       border-bottom-left-radius: $border-radius;
       border-bottom-right-radius: $border-radius;
     }
   }
+
   > .content {
     padding: 8px;
   }
