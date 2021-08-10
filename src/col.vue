7f1c6fafb6110772aea7bda9bbfaa4b9e2265e9f
Xin shen <363088847@qq.com>
2021-08-10 22:53:39 +0800

    创建官网

--- src/col.vue
+++ src/col.vue
@@ -8,14 +8,14 @@ let validator = (value) => {
   let keys = Object.keys(value)
   let valid = true
   keys.forEach(key => {
-    if (!["span", "offset"].includes(key)) {
+    if (!['span', 'offset'].includes(key)) {
       valid = false
     }
   })
   return valid
 }
 export default {
-  name: "GuluCol",
+  name: 'GuluCol',
   props: {
     span: {
       type: [Number, String]
@@ -28,13 +28,13 @@ export default {
     pc: {type: Object, validator,},
     widePc: {type: Object, validator,}
   },
-  data() {
+  data () {
     return {
       gutter: 0,
     }
   },
   methods: {
-    createClasses(obj, str = "") {
+    createClasses (obj, str = '') {
       if (!obj) {return []}
       let array = []
       if (obj.span) { array.push(`col-${str}${obj.span}`) }
@@ -43,66 +43,65 @@ export default {
     }
   },
   computed: {
-    colClass() {
+    colClass () {
       let {span, offset, ipad, narrowPc, pc, widePc} = this
       let createClasses = this.createClasses
       return [
         ...createClasses({span, offset}),
-        ...createClasses(ipad, "ipad-"),
-        ...createClasses(narrowPc, "narrow-pc-"),
-        ...createClasses(pc, "pc-"),
-        ...createClasses(widePc, "wide-pc-"),
+        ...createClasses(ipad, 'ipad-'),
+        ...createClasses(narrowPc, 'narrow-pc-'),
+        ...createClasses(pc, 'pc-'),
+        ...createClasses(widePc, 'wide-pc-'),
       ]
     },
-    colStyle() {
+    colStyle () {
       return {
-        paddingLeft: this.gutter / 2 + "px",
-        paddingRight: this.gutter / 2 + "px",
+        paddingLeft: this.gutter / 2 + 'px',
+        paddingRight: this.gutter / 2 + 'px',
       }
     }
   }
 }
 </script>
 <style scoped lang="scss">
-@use "sass:math";
 .col {
   $class-prefix: col-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
-      width: (math.div($n,24)) * 100%;
+      width: ($n / 24) * 100%;
     }
   }
   $class-prefix: offset-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
-      margin-left: (math.div($n,24)) * 100%;
+      margin-left: ($n / 24) * 100%;
     }
   }
   @media (min-width: 577px) {
     $class-prefix: col-ipad-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
-        width: (math.div($n,24)) * 100%;
+        width: ($n / 24) * 100%;
       }
     }
     $class-prefix: offset-ipad-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
-        margin-left: (math.div($n,24))* 100%;
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
-  @media (min-width: 769px) { // 770
+  @media (min-width: 769px){ // 770
     $class-prefix: col-narrow-pc-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
-        width: (math.div($n,24)) * 100%;
+        width: ($n / 24) * 100%;
       }
     }
     $class-prefix: offset-narrow-pc-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
-        margin-left: (math.div($n,24)) * 100%;
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
@@ -110,13 +109,13 @@ export default {
     $class-prefix: col-pc-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
-        width: (math.div($n,24)) * 100%;
+        width: ($n / 24) * 100%;
       }
     }
     $class-prefix: offset-pc-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
-        margin-left: (math.div($n,24)) * 100%;
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
@@ -124,13 +123,13 @@ export default {
     $class-prefix: col-wide-pc-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
-        width: (math.div($n,24)) * 100%;
+        width: ($n / 24) * 100%;
       }
     }
     $class-prefix: offset-wide-pc-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
-        margin-left: (math.div($n,24)) * 100%;
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
